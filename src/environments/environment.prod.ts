2f2d76890901e3b2fd94956794a2102c5601e392
MariscalDev <mariscaldev@gmx.com>
2026-06-21 00:59:50 -0400

    fix(env): add /v1 to apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://alertasongbackend-production.up.railway.app/api'
+  apiUrl: 'https://alertasongbackend-production.up.railway.app/api/v1'
 };
