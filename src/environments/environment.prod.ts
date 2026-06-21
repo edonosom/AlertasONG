48099576a5498757e3c3e1b2235f577463fa3aab
MariscalDev <mariscaldev@gmx.com>
2026-06-21 01:05:25 -0400

    fix(env): update railway domain to include -3be8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://alertasongbackend-production.up.railway.app/api/v1'
+  apiUrl: 'https://alertasongbackend-production-3be8.up.railway.app/api/v1'
 };
