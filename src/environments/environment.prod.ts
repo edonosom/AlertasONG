e63926139c74f1cd77e02bcf9788276a42c46830
MariscalDev <mariscaldev@gmx.com>
2026-06-21 00:50:09 -0400

    Remove /v1 from environment urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://alertasongbackend-production.up.railway.app/api/v1'
+  apiUrl: 'https://alertasongbackend-production.up.railway.app/api'
 };
